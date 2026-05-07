cbb86b9023d3b4e744fb80145470caa306ec437b
originateai <james.storey@outlook.com.au>
2026-05-07 17:05:15 +1000

    Delete netlify/functions/config.js

--- netlify/functions/config.js
+++
@@ -1,4 +0,0 @@
-var SUPABASE_CONFIG = {
-  url: 'https://hvksgccijlopabnvdkyo.supabase.co',
-  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2a3NnY2NpamxvcGFibnZka3lvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1MzkwNzksImV4cCI6MjA5MjExNTA3OX0.8btaCIg2OMKWdAdFk802QR9Fvcgkr0usgJpMRIkx00I'
-};
